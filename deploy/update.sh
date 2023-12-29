f07ce865e76f0c62cdf1baf83de7241748923191
habibahmadi <habibhussainahmadii@gmail.com>
2023-12-29 03:00:41 -0600

    nothing

--- deploy/update.sh
+++ deploy/update.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/pass_manager'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 cd $PROJECT_BASE_PATH
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart pass_manager_app
+supervisorctl restart profiles_api
 
 echo "DONE! :)"
